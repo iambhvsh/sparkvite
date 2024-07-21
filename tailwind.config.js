3d5476ca4f3bd44b9856655f361b93ed2220fd3c
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-21 11:20:33 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: [
+    './index.html',
+    './pages/**/*.html'
+  ],
   theme: {
     extend: {},
   },
